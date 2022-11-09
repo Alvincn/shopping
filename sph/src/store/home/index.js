55c8c2eda6792d6f9054c924dbd8d1dcb3a968e7
Alvincn <2289530573@qq.com>
2022-11-09 20:31:43 +0800

    day03

--- sph/src/store/home/index.js
+++ sph/src/store/home/index.js
@@ -1,12 +1,22 @@
 import { reqCategoryList } from '@/api/index.js';
 
 // Home 模块的仓库
-const state = {};
-const mutations = {};
+const state = {
+  // 初始值根据获得的数据设置类型
+  categoryList: [],
+};
+const mutations = {
+  CATEGORYLIST(state, categoryList) {
+    let result = categoryList.pop();
+    state.categoryList = categoryList;
+  },
+};
 const actions = {
-  async categoryList() {
+  async categoryList({ commit }) {
     let result = await reqCategoryList();
-    console.log(result);
+    if (result.code == 200) {
+      commit('CATEGORYLIST', result.data);
+    }
   },
 };
 const getters = {};
